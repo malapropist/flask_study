fb70d5172b95535d71bd21b84b61ddb1f48765fc
Josiah Luna <josiah.luna@live.com>
2024-10-10 13:53:01 -0700

    update templates and backend to prevent viewing when not logged in

--- website/auth.py
+++ website/auth.py
@@ -2,20 +2,34 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    email = request.form.get('email')
-    print(email)
-    password = request.form.get('password')
-    print(password)
-    return render_template("login.html", boolean="Swagmoneys")
+    if request.method == 'POST':    
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for("views.home"))
+            else:
+                flash('Incorrect password', category='error')
+        else:
+            flash('Incorrect email.', category='error')
+
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template("logout.html")
+    logout_user()
+    return redirect(url_for("auth.login"))
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
@@ -25,9 +39,11 @@ def sign_up():
         lname = request.form.get('last_name')
         p1 = request.form.get('password1')
         p2 = request.form.get('password2')
-        if User.query.filter_by(email=email).first():
-            print(User.query.filter_by(email=email).first().email)
-            flash('Email is already used.')
+        
+        user = User.query.filter_by(email=email).first()
+        
+        if user:
+            flash(f'\'{user.email}\' is already used.')
         elif len(email) < 1:
             flash('Email must be longer than 4 char', category='error')
             flash('good job nerd', category='success')
@@ -37,7 +53,6 @@ def sign_up():
             flash('pwds dfon\'t match', category='error')
         else:
             # add user to database
-            
             new_user = User(email=email, first_name=fname, password=generate_password_hash(
                 p1, method='pbkdf2:sha256:600000'))
             db.session.add(new_user)
@@ -45,4 +60,4 @@ def sign_up():
             flash('good job nerd', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_user, login_required, logout_user, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        note = request.form.get("note")
+        if len(note)<1:
+            flash('Enter a note', category='error')
+        else:
+            flash('Added note to database', category='success')
+            new_note = Note(data=note, user_id = current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+    return render_template("home.html", user=current_user)
