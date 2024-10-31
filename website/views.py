7fee4c1771ee8aedde0432cbba0bd638033ca55e
Josiah Luna <josiah.luna@live.com>
2024-10-31 13:55:01 -0700

    add page for verseus test

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, g
 from flask_login import login_user, login_required, logout_user, current_user
 from .models import Note
 from . import db
@@ -19,6 +19,18 @@ def home():
             db.session.add(new_note)
             db.session.commit()
     return render_template("home.html", user=current_user)
+
+@views.route('/verses/<int:noteID>', methods=['GET','POST'])
+@login_required
+def verse_select(noteID):
+    # get all notes
+    notes_for_user = Note.query.filter(Note.user_id==current_user.id)
+    # select single note which user chose
+    note_from_id = notes_for_user.filter(Note.id==noteID)
+    
+    # perform verseus logic on it
+
+    return render_template("select_verse.html", user=current_user,notes=notes)
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
